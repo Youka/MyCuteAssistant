292293257e2c0a21fe18517e77ad526f1fb4e7d1
Youka <spanknebel.borken@t-online.de>
2014-11-24 16:24:38 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+MyCuteAssistant
+=========================================
+Your cute girl-for-all on your screen. Let her plan, automate & assist by jobs, making your life easier.
---
+++ src/version.h
@@ -0,0 +1,21 @@
+/*
+Project: MyCuteAssistant
+File: version.h
+
+Copyright (c) 2014, Christoph "Youka" Spanknebel
+
+This software is provided 'as-is', without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
+
+	The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+	Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+	This notice may not be removed or altered from any source distribution.
+*/
+
+#define APP_NAME "MyCuteAssistant"
+#define APP_AUTHOR "Youka"
+#define APP_VERSION_MAJOR 0
+#define APP_VERSION_MINOR 0
+#define APP_VERSION_PATCH 1
+#define APP_VERSION_STRING "" #APP_VERSION_MAJOR "." #APP_VERSION_MINOR "." #APP_VERSION_PATCH
