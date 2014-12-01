81e527e4fe3aa0e0db132e7c481a9e316874f102
Youka <spanknebel.borken@t-online.de>
2014-12-01 01:52:51 +0100

    updated plans; made menu more flexible by show & hide

--- src/TrayIcon.cpp
+++ src/TrayIcon.cpp
@@ -22,18 +22,38 @@ Permission is granted to anyone to use this software for any purpose, including 
 #define IMPORT_RESOURCE_IMAGE(name) extern "C" const unsigned char name[]; extern "C" const unsigned name##_size;
 IMPORT_RESOURCE_IMAGE(logo_ico)
 IMPORT_RESOURCE_IMAGE(bye_png)
-IMPORT_RESOURCE_IMAGE(lone_png)
+IMPORT_RESOURCE_IMAGE(show_hide_png)
 #define QICON(image) QIcon(QPixmap::fromImage(QImage::fromData(image, image##_size)))
 
 TrayIcon::TrayIcon(QWidget* parent) : QSystemTrayIcon(QICON(logo_ico), parent){
 	// Set tray icon properties
 	this->setToolTip(APP_NAME " v" APP_VERSION_STRING);
-	// Set tray icon action
-	QObject::connect(this, &QSystemTrayIcon::activated, [=](QSystemTrayIcon::ActivationReason reason){if(reason == QSystemTrayIcon::ActivationReason::DoubleClick) parent->activateWindow();});
 	// Add tray icon menu
 	QMenu* tray_menu = new QMenu(parent);
-	tray_menu->addAction(QICON(lone_png), "*lone*", parent, SLOT(raise()));
+	QAction* tray_menu_first_item = tray_menu->addAction(QICON(show_hide_png), "");	// Set dynamically (see below)
 	tray_menu->addSeparator();
 	tray_menu->addAction(QICON(bye_png), "Bye-bye", parent, SLOT(close()));
 	this->setContextMenu(tray_menu);
+	// Set tray icon action
+	QObject::connect(this, &QSystemTrayIcon::activated, [=](QSystemTrayIcon::ActivationReason reason){
+		switch(reason){
+			case QSystemTrayIcon::ActivationReason::DoubleClick:
+				parent->activateWindow();
+				break;
+			case QSystemTrayIcon::ActivationReason::Context:
+				if(parent->isVisible() && tray_menu_first_item->text() != "*Crouch*"){
+					tray_menu_first_item->setText("*Crouch*");
+					tray_menu_first_item->disconnect(parent);
+					parent->connect(tray_menu_first_item, SIGNAL(triggered()), SLOT(hide()));
+				}else if(!parent->isVisible() && tray_menu_first_item->text() != "*Emerge*"){
+					tray_menu_first_item->setText("*Emerge*");
+					tray_menu_first_item->disconnect(parent);
+					parent->connect(tray_menu_first_item, SIGNAL(triggered()), SLOT(show()));
+				}
+				break;
+			case QSystemTrayIcon::ActivationReason::Unknown:
+			case QSystemTrayIcon::ActivationReason::Trigger:
+			case QSystemTrayIcon::ActivationReason::MiddleClick: break;
+		}
+	});
 }
